1be1b3091dceb9c0a617f7f8ff9adb4c5271e5b7
tonygarza18995-max <tonygarza18995@gmail.com>
2026-06-15 19:32:39 -0600

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: version: "1.0.28",
+  version: "1.0.28",
 ,
   orientation: "portrait",
   icon: "./assets/images/icon.png",
